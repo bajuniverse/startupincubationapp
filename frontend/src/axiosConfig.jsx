0992d8c80ed178c7bcbb15974f871dd4f99cc808
Narciso Jr Batingal <batingalnarz11@gmail.com>
2025-08-10 23:43:25 +1000

    fix(frontend): axiosConfig

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://localhost:5001', // local ..shuda updated
   // baseURL: 'http://54.79.212.150:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
