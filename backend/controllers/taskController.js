93838fd624c04c84feaa18ffbbbcda95e99ec7d8
Narciso Jr Batingal <batingalnarz11@gmail.com>
2025-08-13 00:59:53 +1000

    feat(tasks): add taskmanager components and api

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -23,14 +23,14 @@ const addTask = async (req, res) => {
 
 // Update Task
 const updateTask = async (req, res) => {
-    const { title, description, completed, deadline } = req.body;
+    const { title, description, deadline } = req.body;
     try {
         const task = await Task.findById(req.params.id);
         if (task) return res.status(404).json({ message: 'Task not found' });
         
         task.title = title || task.title;
         task.description = description || task.description;
-        task.completed = completed ?? task.completed;
+        // task.completed = completed || task.completed;
         task.deadline = deadline || task.deadline;
 
         const updatedTask = await task.save();
