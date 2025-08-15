df76ad220cb11d6b64f11b4c6db5da0478db1da7
Narciso Jr Batingal <batingalnarz11@gmail.com>
2025-08-15 15:06:26 +1000

    ci(workflow): fix change

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
-      <Link to="/" className="text-2xl font-bold">SIMS</Link>
+      <Link to="/" className="text-2xl font-bold">Startup Incubation</Link>
       <div>
         {user ? (
           <>
